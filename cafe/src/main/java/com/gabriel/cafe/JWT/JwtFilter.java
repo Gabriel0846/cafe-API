804be0e0511354406d3f279b3664d16ef79263d3
Gabriel Lopes <carnagesagas@gmail.com>
2024-03-27 08:19:09 -0300

    jwt filter

--- cafe/src/main/java/com/gabriel/cafe/JWT/JwtFilter.java
+++ cafe/src/main/java/com/gabriel/cafe/JWT/JwtFilter.java
@@ -8,8 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter{
@@ -20,13 +26,47 @@ public class JwtFilter extends OncePerRequestFilter{
     @Autowired
     private CustomerUsersDetailsService service;
 
+    Claims claims = null;
+    private String userName = null;
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
         if(httpServletRequest.getServletPath().matches("/usuario/login|/usuario/esqueceu_senha|/usuario/cadastrar")) {
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         } else {
-            
+            String authorizationHeader = httpServletRequest.getHeader("authorization");
+            String token = null;
+
+            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+                token = authorizationHeader.substring(7);
+                userName = jwtUtil.extractUsername(token);
+                claims = jwtUtil.extractAllClaims(token);
+            }
+
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication()==null) {
+                UserDetails userDetails = service.loadUserByUsername(userName);
+                if (jwtUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
         }
+    }
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public boolean isUser() {
+        return "user".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    public String getCurrentUser(){
+        return userName;
     }
 }
