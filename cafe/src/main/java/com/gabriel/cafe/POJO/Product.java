4c6a1d6c5d94f2a815b06a116181e7a4b2b5eadf
Gabriel Lopes <carnagesagas@gmail.com>
2024-06-12 16:09:03 -0300

    classe de produto

---
+++ cafe/src/main/java/com/gabriel/cafe/POJO/Product.java
@@ -0,0 +1,49 @@
+package com.gabriel.cafe.POJO;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.Data;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "Produto")
+public class Product implements Serializable{
+    
+    public static final Long SerialVersionUid = 123456L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "categoria_fk", nullable = false)
+    private Category category;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
+    private Integer preco;
+
+    @Column(name = "status")
+    private String status;
+}
